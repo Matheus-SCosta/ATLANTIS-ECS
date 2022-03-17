65c5b96a80e56eea42eb82f439f1d6b444d9602a
Matheus-SCosta <silvadacosta2018@gmail.com>
2022-03-17 16:52:23 -0300

    s

--- data.tf
+++ data.tf
@@ -6,15 +6,15 @@ data "aws_vpc" "vpc_default" {
   cidr_block = "172.31.0.0/16"
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners      = ["self"]
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}  
+#data "aws_ami" "ubuntu" {
+#  most_recent = true
+#  owners      = ["self"]
+#  filter {
+#    name   = "name"
+#    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+#  }
+#  filter {
+#    name   = "virtualization-type"
+#    values = ["hvm"]
+#  }
+#}  

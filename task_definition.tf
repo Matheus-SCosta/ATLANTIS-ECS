4c2d15a35258ad5f7dcd4953789fc2e794fa7aff
Matheus-SCosta <silvadacosta2018@gmail.com>
2022-03-18 13:51:30 -0300

    s

--- task_definition.tf
+++ task_definition.tf
@@ -15,3 +15,15 @@ resource "aws_ecs_task_definition" "atlantis" {
     }
   ])
 }
+
+resource "aws_ecs_task_set" "atlantis" {
+  service         = aws_ecs_service.atlantis.id
+  cluster         = aws_ecs_cluster.atlantis.id
+  task_definition = aws_ecs_task_definition.atlantis.arn
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.tg_ecs.arn
+    container_name   = "atlantis_container"
+    container_port   = 4141
+  }
+}

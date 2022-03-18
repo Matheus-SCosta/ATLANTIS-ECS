e2204bf827a704992408b5049befeec75fb874fc
Matheus-SCosta <silvadacosta2018@gmail.com>
2022-03-18 15:28:16 -0300

    s

--- launch_configuration.tf
+++ launch_configuration.tf
@@ -2,6 +2,6 @@ resource "aws_launch_configuration" "ubuntu_v2" {
   name          = "atlantis_ecs_v2"
   image_id      = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
-  user_data     = file("user_data.sh")
+  user_data     = file("./user_data.sh")
 
 }

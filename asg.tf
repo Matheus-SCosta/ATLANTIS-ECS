40c4028f5dd19bd3598eed4269408d7351199327
Matheus-SCosta <silvadacosta2018@gmail.com>
2022-03-18 18:02:44 -0300

    s

--- asg.tf
+++ asg.tf
@@ -2,9 +2,9 @@ resource "aws_autoscaling_group" "atlantis_ecs" {
   name                      = "${var.resource_name}_ecs"
   max_size                  = 2
   min_size                  = 2
-  health_check_grace_period = 2
+  health_check_grace_period = 1
   health_check_type         = "EC2"
-  desired_capacity          = 1
+  desired_capacity          = 2
   force_delete              = true
   launch_configuration      = aws_launch_configuration.amazon_linux.name
   vpc_zone_identifier       = var.subnets_lb

5c5b7627142eb9eee49d1ac09043cbea27e31fb8
Matheus-SCosta <silvadacosta2018@gmail.com>
2022-03-18 13:34:34 -0300

    s

---
+++ task_definition.tf
@@ -0,0 +1,17 @@
+resource "aws_ecs_task_definition" "atlantis" {
+  family = "atlantis"
+  container_definitions = jsonencode([
+    {
+      name      = "atlantis"
+      image     = "398963803929.dkr.ecr.us-east-2.amazonaws.com/atlantis:2.0"
+      cpu       = 128
+      memory    = 512
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+        }
+      ]
+    }
+  ])
+}

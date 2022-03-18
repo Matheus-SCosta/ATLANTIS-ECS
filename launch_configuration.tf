d8d2d5caa3f7f3f1a68762de93538f22e3be80d0
Matheus-SCosta <silvadacosta2018@gmail.com>
2022-03-18 15:01:20 -0300

    s

--- launch_configuration.tf
+++ launch_configuration.tf
@@ -1,3 +1,10 @@
+resource "aws_launch_configuration" "ubuntu_20_04" {
+  name          = "atlantis_ecs"
+  image_id      = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+ 
+}
+
 resource "aws_launch_configuration" "ubuntu" {
   name          = "atlantis_ecs"
   image_id      = data.aws_ami.ubuntu.id

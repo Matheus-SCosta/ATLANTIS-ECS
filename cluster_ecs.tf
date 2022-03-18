2617c20b0bddb3dac031f875a2d7f8888e0b1f2e
Matheus-SCosta <silvadacosta2018@gmail.com>
2022-03-18 15:45:57 -0300

    s

--- cluster_ecs.tf
+++ cluster_ecs.tf
@@ -1,24 +1,24 @@
-resource "aws_ecs_cluster" "atlantis" {
-  name               = "atlantis"
-  capacity_providers = [aws_ecs_capacity_provider.asg_atlantis.name]
+#resource "aws_ecs_cluster" "atlantis" {
+#  name               = "atlantis"
+#  capacity_providers = [aws_ecs_capacity_provider.asg_atlantis.name]
 
-  default_capacity_provider_strategy {
-    base              = 1
-    weight            = 10
-    capacity_provider = aws_ecs_capacity_provider.asg_atlantis.name
-  }
+#  default_capacity_provider_strategy {
+#    base              = 1
+#    weight            = 10
+#    capacity_provider = aws_ecs_capacity_provider.asg_atlantis.name
+#  }
 
-  setting {
-    name  = "containerInsights"
-    value = "enabled"
-  }
-}
+#  setting {
+#    name  = "containerInsights"
+#    value = "enabled"
+#  }
+#}
 
 
-resource "aws_ecs_capacity_provider" "asg_atlantis" {
-  name = "capacity_asg_atlantis"
+#resource "aws_ecs_capacity_provider" "asg_atlantis" {
+#  name = "capacity_asg_atlantis"
 
-  auto_scaling_group_provider {
-    auto_scaling_group_arn = aws_autoscaling_group.atlantis_ecs.arn
-  }
-}
+#  auto_scaling_group_provider {
+#    auto_scaling_group_arn = aws_autoscaling_group.atlantis_ecs.arn
+#  }
+#}
--- service_ecs.tf
+++ service_ecs.tf
@@ -1,14 +1,14 @@
-resource "aws_ecs_service" "atlantis" {
-  name            = "atlantis"
-  cluster         = aws_ecs_cluster.atlantis.id
-  task_definition = aws_ecs_task_definition.atlantis.arn
-  desired_count   = 1
+#resource "aws_ecs_service" "atlantis" {
+#  name            = "atlantis"
+#  cluster         = aws_ecs_cluster.atlantis.id
+#  task_definition = aws_ecs_task_definition.atlantis.arn
+#  desired_count   = 1
 
 
-  load_balancer {
-    target_group_arn = aws_lb_target_group.tg_ecs.arn
-    container_name   = "atlantis_container"
-    container_port   = 4141
-  }
+#  load_balancer {
+#    target_group_arn = aws_lb_target_group.tg_ecs.arn
+#    container_name   = "atlantis_container"
+#    container_port   = 4141
+#  }
 
-}
+#}

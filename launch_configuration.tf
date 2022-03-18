26df1f039b2f2fae4e4bc40391d750fda9fdcf46
Matheus-SCosta <silvadacosta2018@gmail.com>
2022-03-18 15:38:14 -0300

    s

--- launch_configuration.tf
+++ launch_configuration.tf
@@ -2,6 +2,8 @@ resource "aws_launch_configuration" "ubuntu_v2" {
   name          = "atlantis_ecs_v2"
   image_id      = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
+  user_data     = "#!/bin/bash\necho ECS_CLUSTER=atlantis >> /etc/ecs/ecs.config"
+
 }
 
 resource "aws_launch_configuration" "ubuntu" {

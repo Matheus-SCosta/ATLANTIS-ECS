645e14ce9ea058e7a214753551f19aac65a86e0d
Matheus-SCosta <silvadacosta2018@gmail.com>
2022-03-18 15:35:33 -0300

    s

--- asg.tf
+++ asg.tf
@@ -15,6 +15,7 @@ resource "aws_autoscaling_group" "atlantis_ecs" {
     propagate_at_launch = true
   }
 
+    
   timeouts {
     delete = "15m"
   }
--- launch_configuration.tf
+++ launch_configuration.tf
@@ -3,5 +3,6 @@ resource "aws_launch_configuration" "ubuntu_v2" {
   image_id      = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   user_data     = file("user_data.sh")
+  security_groups    = [data.aws_security_group.sg_ecs.id]
 
 }

f9c7212428c29b29b444334b1a302fbd784c60a2
Matheus-SCosta <silvadacosta2018@gmail.com>
2022-03-18 12:28:58 -0300

    s

--- alb.tf
+++ alb.tf
@@ -21,13 +21,13 @@ resource "aws_lb" "lb_ecs" {
   }
 }
 
-#resource "aws_lb_listener" "front_end" {
-#  load_balancer_arn = aws_lb.lb_ecs.arn
-#  port              = "80"
-#  protocol          = "HTTP"
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.lb_ecs.arn
+  port              = "80"
+  protocol          = "HTTP"
 
-#  default_action {
-#    type             = "forward"
-#    target_group_arn = aws_lb_target_group.tg_ecs.arn
-#  }
-#}
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg_ecs.arn
+  }
+}

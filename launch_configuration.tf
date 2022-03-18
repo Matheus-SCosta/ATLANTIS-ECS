c53afc32328e4bcf2644b48f7305e209c01936fa
Matheus-SCosta <silvadacosta2018@gmail.com>
2022-03-18 16:24:00 -0300

    s

--- launch_configuration.tf
+++ launch_configuration.tf
@@ -1,11 +1,11 @@
-resource "aws_launch_configuration" "ubuntu_v3" {
-  name          = "atlantis_ecs_v3"
-  image_id      = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-  user_data     = "#!/bin/bash\necho ECS_CLUSTER=my-cluster >> /etc/ecs/ecs.config"
-  security_groups    = [data.aws_security_group.sg_ecs.id]
-  key_name = "SNS"
-}
+#resource "aws_launch_configuration" "ubuntu_v3" {
+#  name          = "atlantis_ecs_v3"
+#  image_id      = data.aws_ami.ubuntu.id
+#  instance_type = "t2.micro"
+#  user_data     = "#!/bin/bash\necho ECS_CLUSTER=my-cluster >> /etc/ecs/ecs.config"
+#  security_groups    = [data.aws_security_group.sg_ecs.id]
+#  key_name = "SNS"
+#}
 
 
 resource "aws_launch_configuration" "ubuntu" {

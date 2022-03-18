a60bce67e53b4835cf151728cf6ed11d7602f3a1
Matheus-SCosta <silvadacosta2018@gmail.com>
2022-03-18 11:21:52 -0300

    s

--- cluster_ecs.tf
+++ cluster_ecs.tf
@@ -1,17 +1,18 @@
-#resource "aws_ecs_cluster" "atlantis" {
-#  name = "atlantis"
+resource "aws_ecs_cluster" "atlantis" {
+  name = "atlantis"
+  capacity_providers = [aws_ecs_capacity_provider.asg_atlantis.name]
 
-#  setting {
-#    name  = "containerInsights"
-#    value = "enabled"
-#  }
-#}
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+}
 
 
-#resource "aws_ecs_capacity_provider" "asg_atlantis" {
-#  name = "example"
+resource "aws_ecs_capacity_provider" "asg_atlantis" {
+  name = "capacity_asg_atlantis"
 
-#  auto_scaling_group_provider {
-#    auto_scaling_group_arn = aws_autoscaling_group.atlantis_ecs.arn
-#  }
-#}
+  auto_scaling_group_provider {
+    auto_scaling_group_arn = aws_autoscaling_group.atlantis_ecs.arn
+  }
+}

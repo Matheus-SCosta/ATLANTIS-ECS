e07da2747b697815ed66cdf7e106ab707a8884c8
Matheus-SCosta <silvadacosta2018@gmail.com>
2022-03-18 17:03:07 -0300

    s

--- launch_configuration.tf
+++ launch_configuration.tf
@@ -9,12 +9,3 @@ resource "aws_launch_configuration" "amazon_linux" {
 }
 
 
-resource "aws_launch_configuration" "ubuntu" {
-  name          = "atlantis_ecs"
-  image_id      = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-  user_data     = "#!/bin/bash\necho ECS_CLUSTER=my-cluster >> /etc/ecs/ecs.config"
-  security_groups    = [data.aws_security_group.sg_ecs.id]
-  key_name = "SNS"
-}
-
